6c8030b15956853b19ae04e061741932c9de3817
seung.bok <seungbok.lee@huinnoaim.com>
2023-08-21 16:11:04 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/elemental/ElementalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elemental
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElementalApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElementalApplication>(*args)
+}
